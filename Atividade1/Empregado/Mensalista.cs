dfff198c6fc6259a167d8afa77bd3ce0a9c468e1
GUSTAVO HENRIQUE OLIVEIRA DA SILVA <230210@lti.fatecso.edu.br>
2026-03-18 17:37:00 -0300

    atividade 1

---
+++ Atividade1/Empregado/Mensalista.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Empregado
+{
+    internal class Mensalista : Empregado
+    {
+        public double SalarioMensal {  get; set; }
+
+        public override double SalarioBruto()
+        {
+            return SalarioMensal;
+        }
+
+        public Mensalista() //construtor
+        {
+            System.Windows.Forms.MessageBox.Show("aqui é Mensalista");
+        }
+
+        public Mensalista(int matx, string nomex, DateTime datax, double salx)
+        {
+            this.NomeEmpregado = nomex; 
+            this.Matricula = matx;
+            this.DataEntradaEmpresa = datax;
+            this.SalarioMensal = salx;
+        }
+    }
+}
