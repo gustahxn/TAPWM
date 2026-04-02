299138dc5ef5a212001436871ab4ff1be38d7e62
gustahxn <guenrik2007@gmail.com>
2026-04-02 11:03:36 -0300

    Atividade 3 js - feita

--- Atividade3/js/media.js
+++ Atividade3/js/media.js
@@ -1,26 +1,26 @@
 // calculo
 const calculateGrades = () => {
-  const name = prompt("digite o nome do aluno:");
+  const name = prompt("Digite o nome do aluno:");
 
   // sair se o user cancelar
   if (!name) return;
 
-  const g1 = parseFloat(prompt("digite a 1ª nota:"));
-  const g2 = parseFloat(prompt("digite a 2ª nota:"));
-  const g3 = parseFloat(prompt("digite a 3ª nota:"));
+  const g1 = parseFloat(prompt("Digite a 1ª nota:"));
+  const g2 = parseFloat(prompt("Digite a 2ª nota:"));
+  const g3 = parseFloat(prompt("Digite a 3ª nota:"));
 
   // valida entradas
   if (isNaN(g1) || isNaN(g2) || isNaN(g3)) {
-    alert("notas inválidas inseridas. a operação foi cancelada.");
+    alert("Notas inválidas inseridas. A operação foi cancelada.");
     return;
   }
 
-  // output formatado
+  // output
   const mean = (g1 + g2 + g3) / 3;
-  alert(`aluno: ${name}\nmédia aritmética: ${mean.toFixed(2)}`);
+  alert(`Aluno: ${name}\nMédia aritmética: ${mean.toFixed(2)}`);
 
   // reiniciar
-  const retry = confirm("deseja calcular a média de outro aluno?");
+  const retry = confirm("Deseja calcular a média de outro aluno?");
   if (retry) calculateGrades();
 };
 
--- Atividade3/js/operacoes.js
+++ Atividade3/js/operacoes.js
@@ -1,34 +1,34 @@
 // maths
 const runOperations = () => {
-  const n1 = parseFloat(prompt("digite o primeiro número:"));
-  const n2 = parseFloat(prompt("digite o segundo número:"));
+  const n1 = parseFloat(prompt("Digite o primeiro número:"));
+  const n2 = parseFloat(prompt("Digite o segundo número:"));
 
   // busca por nums validos
   if (isNaN(n1) || isNaN(n2)) {
-    alert("valores inválidos. digite apenas números.");
+    alert("Valores inválidos. Digite apenas números.");
     return;
   }
 
   // impede divisao por zero
   const divResult =
-    n2 !== 0 ? (n1 / n2).toFixed(2) : "erro (divisão por zero não permitida)";
+    n2 !== 0 ? (n1 / n2).toFixed(2) : "Erro (divisão por zero não permitida)";
   const modResult = n2 !== 0 ? n1 % n2 : "erro";
 
   const results = `
-        números escolhidos: ${n1} e ${n2}
+        Números escolhidos: ${n1} e ${n2}
         ------------------------------
-        soma: ${n1 + n2}
-        subtração: ${n1 - n2}
-        produto: ${n1 * n2}
-        divisão: ${divResult}
-        resto da divisão: ${modResult}
+        Soma: ${n1 + n2}
+        Subtração: ${n1 - n2}
+        Produto: ${n1 * n2}
+        Divisão: ${divResult}
+        Resto da divisão: ${modResult}
     `;
 
   // exibe resultados
   alert(results.trim().replace(/^ +/gm, ""));
 
   // reiniciar
-  const retry = confirm("deseja realizar novos cálculos?");
+  const retry = confirm("Deseja realizar novos cálculos?");
   if (retry) runOperations();
 };
 
