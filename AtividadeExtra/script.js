19680f118296e90db7ce2717716181609f9377a5
Gustavo da Silva <129549906+gustahxn@users.noreply.github.com>
2026-05-20 18:04:37 -0300

    correção jeito 4

--- AtividadeExtra/script.js
+++ AtividadeExtra/script.js
@@ -3,7 +3,38 @@ const pessoa1 = {
     nome: 'Gustavo',
     idade: 18,
     hobby: 'Escutar música'
+};//jeito 1
+const pessoa1 = {
+    nome: 'Gustavo',
+    idade: 18,
+    hobby: 'Escutar música'
 };
+
+//jeito 2
+const pessoa2 = new Object();
+
+pessoa2.nome = 'Gustavo';
+pessoa2.idade = 18;
+pessoa2.hobby = 'Jogar videogames';
+
+//jeito 3
+const pessoa3 = Object.create(objOrg, { 
+    nome: { value: 'Gustavo'},
+    idade: { value: 18},
+    hobby: { value: 'Caminhar'}
+});
+
+//jeito 4
+function Pessoa(nomePessoa, idadePessoa, hobbyPessoa) {
+    this.nome = nomePessoa;
+    this.idade = idadePessoa;
+    this.hobby = hobbyPessoa;
+}
+
+const pessoa4 = new Pessoa('Gustavo', 18, 'Tocar guitarra');
+
+//jeito 5
+
 
 //jeito 2
 const pessoa2 = new Object();
