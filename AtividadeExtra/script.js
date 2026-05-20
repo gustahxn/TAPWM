7fa950c876c667346f95e85aab5ba48ba3a4417d
Gustavo da Silva <129549906+gustahxn@users.noreply.github.com>
2026-05-20 17:58:24 -0300

    att extra

---
+++ AtividadeExtra/script.js
@@ -0,0 +1,29 @@
+//jeito 1
+const pessoa1 = {
+    nome: 'Gustavo',
+    idade: 18,
+    hobby: 'Escutar música'
+};
+
+//jeito 2
+const pessoa2 = new Object();
+
+pessoa2.nome = 'Gustavo';
+pessoa2.idade = 18;
+pessoa2.hobby = 'Jogar videogames';
+
+//jeito 3
+const pessoa3 = Object.create(objOrg, { 
+    nome: { value: 'Gustavo'},\\
+    idade: { value: 18},
+    hobby: { value: 'Caminhar'}
+});
+
+//jeito 4
+function criarPessoa(nomePessoa, idadePessoa, hobbyPessoa) {
+    this.nome = nomePessoa;
+    this.idade = idadePessoa;
+    this.hobby = hobbyPessoa;
+}
+
+const pessoa4 = new Pessoa4('Gustavo', 18, 'Tocar guitarra');
