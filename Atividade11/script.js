29bedb0e5cb0c484c84573d4f480e89e075e6abd
gustahxn <guenrik2007@gmail.com>
2026-05-21 00:25:25 -0300

    Atividade 11 - ex do retangulo pronto

---
+++ Atividade11/script.js
@@ -0,0 +1,38 @@
+//puxa e declara os dados via id
+const alturaInput = document.getElementById("alturaInput");
+const baseInput = document.getElementById("baseInput");
+const botaoEnviar = document.getElementById("botaoEnviar");
+const outputDiv = document.getElementById("outputDiv");
+const botaoLimpar = document.getElementById("botaoLimpar");
+
+//Ex 1
+function Retangulo(b, a) {
+  this.b = b;
+  this.a = a;
+
+  this.calcularArea = function () {
+    return this.b * this.a; //usando this para pegar os valores ja passados
+  };
+}
+//this se usa dentro da função construtora, mas não fora dela
+
+//botao limpar
+botaoLimpar.addEventListener("click", function () {
+  alturaInput.value = "";
+  baseInput.value = "";
+  outputDiv.innerHTML = "";
+});
+
+//botao Enviar
+botaoEnviar.addEventListener("click", function () {
+  const b = Number(baseInput.value);
+  const a = Number(alturaInput.value);
+
+  //usa o Retangulo para criar o objeto ret
+  const ret = new Retangulo(b, a);
+  //usa o metodo de calcular e armazena o resultado
+  const area = ret.calcularArea();
+
+  outputDiv.innerHTML = "A área do retângulo é: " + area;
+  console.log("A área do retângulo é: " + area); //exibindo tanto no console quanto no HTML
+});
