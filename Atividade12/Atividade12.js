2259653aa2718b23aa7b25cf334c625fcc1b18c5
gustahxn <guenrik2007@gmail.com>
2026-05-27 20:31:02 -0300

    Atividade 12 - refac para suportar a volta dos movimentos do mouse apos botao de rest - usando funcoes

--- Atividade12/Atividade12.js
+++ Atividade12/Atividade12.js
@@ -1,33 +1,41 @@
 //declarando
-const janela = document.getElementById("janelaF");
+const janela = document.getElementById("janela");
 const botaoResetar = document.getElementById("botaoResetar");
 let titulo = "";
 let estaQuebrada = false;
 
-//passar mouse em cima
-janelaF.onmouseover = function () {
-  this.src = "./img/janelaAberta.jpg"; //aplica img
-  document.getElementById("titulo").textContent = "Janela Aberta"; //h1
-};
+//movimentos que o mouse vai fazer
+function eventosMouse() {
+  janela.onmouseover = function () {
+    //passar mouse em cima
+    this.src = "./img/janelaAberta.jpg"; //aplica img
+    document.getElementById("titulo").textContent = "Janela Aberta"; //h1
+  };
 
-//tirar mouse da janela
-janelaF.onmouseout = function () {
-  this.src = "./img/janelaFechada.webp";
-  document.getElementById("titulo").textContent = "Janela Fechada";
-};
+  //tirar mouse da janela
+  janela.onmouseout = function () {
+    this.src = "./img/janelaFechada.webp";
+    document.getElementById("titulo").textContent = "Janela Fechada";
+  };
+
+  janela.onmousedown = function () {
+    this.src = "./img/janelaQuebrada.png";
+    document.getElementById("titulo").textContent = "Janela Quebrada";
+    estaQuebrada = true;
+
+    //desativa hover e out
+    this.onmouseout = null;
+    this.onmouseover = null;
+  };
+}
+
+eventosMouse();
 
 //clickar na janela
-janelaF.onmousedown = function () {
-  this.src = "./img/janelaQuebrada.png";
-  document.getElementById("titulo").textContent = "Janela Quebrada";
-  estaQuebrada = true;
-
-  //desativa hover e out
-  this.onmouseout = null;
-  this.onmouseover = null;
-};
 
 botaoResetar.addEventListener("click", () => {
   janela.src = "./img/janelaFechada.webp";
   document.getElementById("titulo").textContent = "Abra a JANELA";
+  //resetando os movimentos do mouse
+  eventosMouse();
 });
