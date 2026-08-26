bfb126f134e17249ca20d8c2b85367432d6a076b
gustahxn <guenrik2007@gmail.com>
2026-08-26 14:34:16 -0300

    Att 18 - 26/08 - ejs para puxar o html pela pasta views, utilizando res.render

--- Atividade18/Exercicios/app.js
+++ Atividade18/Exercicios/app.js
@@ -1,20 +1,22 @@
 let express  = require('express');
 let app = express(); //executando o express
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req,res) {
-    res.send("<html><body>Site da Fatec Sorocaba</body></html>");
+    res.send("<html><body>Site da Fatec Sorocaba</body></html>"); //sem ejs, utilizando send
 });
 
 app.get('/historia', function(req,res) {
-    res.send("<html><body>Mudança para o server atualizar ++ História da Fatec Sorocaba</body></html>");
+    res.render("secao/historia"); //com ejs, utilizando render e puxando de dentro de views
 });
 
 app.get('/cursos', function(req,res) {
-    res.send("<html><body>Cursos da Fatec Sorocaba</body></html>");
+    res.render("secao/cursos"); //com ejs, utilizando render e puxando de dentro de views
 });-
 
 app.get('/professores', function(req,res) {
-    res.send("<html><body>Professores da Fatec Sorocaba</body></html>");
+    res.render("secao/professores*"); //com ejs, utilizando render e puxando de dentro de views
 });-
 
 
